4ca1bceeab575139a729f8f03bf915b880f0210e
Taufik Pragusga <pragusga25@gmail.com>
2021-10-03 00:34:02 +0700

    change dto validation

--- src/urls/dtos/create-url-dto.ts
+++ src/urls/dtos/create-url-dto.ts
@@ -1,11 +1,8 @@
-import { IsString, Matches } from 'class-validator';
+import { IsString, IsUrl, Matches } from 'class-validator';
 
 export class CreateUrlDto {
   @IsString()
-  @Matches(
-    /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/,
-    { message: 'URL is not valid' },
-  )
+  @IsUrl(undefined, { message: 'URL is not valid' })
   readonly longUrl: string;
 
   @IsString()
